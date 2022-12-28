79efa5f1d446b3f11d9e94b15b1f1261f518285a
chijindu okechukwu <okechukwu008@gmail.com>
2022-12-28 21:15:28 +0100

    Some fixes 2

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,14 +2,14 @@ import BaseHttpService from './base-http.service';
 
 export default class AuthService extends BaseHttpService {
   async signin(username, password) {
-    const result = await this.post(`${this.BASE_URL}/auth/signin`, { username, password });
+    const result = await this.post(`auth/signin`, { username, password });
     const accessToken = result.data?.accessToken;
     this.saveToken(accessToken);
     return result.data?.username;
   }
 
   async signup(username, password) {
-    await this.post(`${this.BASE_URL}/auth/signup`, { username, password });
+    await this.post(`auth/signup`, { username, password });
   }
 
   async signout() {
